d390c98143c5e536c876eeca0dea3caf52510326
Kate Haskell <khaskell@ufl.edu>
2017-04-21 08:32:50 -0400

    NodeJS doesn't need IIFE wrapping

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-(function () {
-  'use strict'
+const R = require('ramda')
 
-  const R = require('ramda')
-
-  const fillArray = (fn, n) => {
-    let arr = []
-    const arrayFill = (fn, n) => {
-      if (n === 1) {
-        return [fn()]
-      }
-      return arr.concat([fn()].concat(arrayFill(fn, n - 1)))
+const fillArray = (fn, n) => {
+  let arr = []
+  const arrayFill = (fn, n) => {
+    if (n === 1) {
+      return [fn()]
     }
-    return arrayFill(fn, n)
+    return arr.concat([fn()].concat(arrayFill(fn, n - 1)))
   }
+  return arrayFill(fn, n)
+}
 
-  const rollDie = max => Math.floor((Math.random() * max) + 1)
-  const rollD6 = () => rollDie(6)
-  const generateRolls = () => fillArray(rollD6, 4)
-  const sortRolls = arr => arr.concat().sort()
-  const dropLow = arr =>
-    sortRolls(arr).slice(1)
-  const sum = arr => arr.reduce((a, b) => a + b, 0)
-  const generateStat = R.pipe(generateRolls, sortRolls, dropLow, sum)
+const rollDie = max => Math.floor((Math.random() * max) + 1)
+const rollD6 = () => rollDie(6)
+const generateRolls = () => fillArray(rollD6, 4)
+const sortRolls = arr => arr.concat().sort()
+const dropLow = arr =>
+sortRolls(arr).slice(1)
+const sum = arr => arr.reduce((a, b) => a + b, 0)
+const generateStat = R.pipe(generateRolls, sortRolls, dropLow, sum)
 
-  const generateStatMatrix = () => fillArray(generateStat, 6)
+const generateStatMatrix = () => fillArray(generateStat, 6)
 
-  console.log(fillArray(generateStatMatrix, 6))
-}())
+console.log(fillArray(generateStatMatrix, 6))
